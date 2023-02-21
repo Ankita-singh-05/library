a4c457bad8b1087db4e524984680331e2419990b
Ankita Bimal Singh <68849496+Ankita-singh-05@users.noreply.github.com>
2023-02-21 20:50:01 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -2,7 +2,7 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','xbkwZSnrv7AKvE');
+define('DB_PASS','SPR3jk4xqWqgwJ');
 define('DB_NAME','library');
 // Establish database connection.
 try
